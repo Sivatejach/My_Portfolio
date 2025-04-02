158fb816e329b855dda737674b40bb7283875c17
Sivatejach <chsteja2003@gmail.com>
2025-04-03 00:34:48 +0530

    Skills Card Updated

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -1,9 +1,8 @@
 const skills = [
     "ReactJS", "JavaScript", "Node.js", "Express.js", "MongoDB",
-    "HTML", "CSS", "Python", "Java", "SQL",
-    "Django", "Redux", "GraphQL", "TypeScript", "TailwindCSS",
-    "AWS", "Docker", "Kubernetes", "Firebase", "Git",
-    "Linux", "Jenkins", "C++", "Swift", "Go"
+    "HTML", "CSS", "Python", "Java", "SQL", "Git",
+    "Linux", "C++","Java","PowerBI","Problem-solving","Collaboration",
+    "VSCode", "Responsive Design", "RESTful APIs",
   ];
   
   export default skills;

a6021fead481dae62b941b11a57fee4ef2f45212
Sivatejach <chsteja2003@gmail.com>
2025-04-04 15:59:56 +0530

    Skills Updted

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -1,8 +1,9 @@
 const skills = [
-    "ReactJS", "JavaScript", "Node.js", "Express.js", "MongoDB",
-    "HTML", "CSS", "Python", "Java", "SQL", "Git",
-    "Linux", "C++","Java","PowerBI","Problem-solving","Collaboration",
-    "VSCode", "Responsive Design", "RESTful APIs",
+    "ReactJS", "Node.js", "Express.js", "MongoDB",
+    "Python", "SQL", "Git",
+    "Linux","Java","PowerBI","Problem-solving","Collaboration",
+    "Teamwork", "Communication",
+    "VSCode", "RESTful APIs",
   ];
   
   export default skills;
